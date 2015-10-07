81c63c61110397b800b1b04276336da21d163728
kartikey <kartikeybharti@gmail.com>
2015-10-06 21:01:38 -0700

    initial commit

---
+++ hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by iGuest on 10/6/15.
+//  Copyright (c) 2015 kartikey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
